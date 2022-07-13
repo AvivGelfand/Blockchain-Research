cbeba44dafc698bcff1f94d679ffbbd93d7db00a
AvivGelfand <63909805+AvivGelfand@users.noreply.github.com>
2022-07-13 20:40:48 +0300

    new changes

--- project/CustomerPreferences.sol
+++ project/CustomerPreferences.sol
@@ -2,23 +2,23 @@
 pragma solidity ^0.8.10;
 
 contract CustomerPreferences {
-    // Eents
+    
+    // Eevents
     event GrantRole(bytes32 indexed role, address indexed account);
-    event RevokeRole(bytes32 indexed role, address indexed account);
+    // event RevokeRole(bytes32 indexed role, address indexed account);
     
     event SetShare(bytes32 indexed share, address indexed account);
 
-    // role => account => bool
-    mapping(bytes32 => mapping ( address => bool) ) public roles;
+    // Prefrences Array: prefersnce => account => bool
+    mapping(bytes32 => mapping ( address => bool) ) public prefrencess;
     // we save gas by mapping the name to numbers
     
-    // define roles, Note that other roles can be made
     
     // 0xdf8b4c520ffe197c5343c6f5aec59570151ef9a492f2c624fd45ddde6135ec42 // private
-    bytes32 private constant ADMIN = keccak256(abi.encodePacked("ADMIN"));
+    // bytes32 private constant ADMIN = keccak256(abi.encodePacked("ADMIN"));
 
 
-    // 0x2db9fd3d099848027c2383d0a083396f6c41510d7acfd92adc99b6cffcf31e96 // private
+    //  // private
     bytes32 private constant USER = keccak256(abi.encodePacked("USER"));
     // USER
     //0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
@@ -27,13 +27,15 @@ contract CustomerPreferences {
     bytes32 public constant COMPANY = keccak256(abi.encodePacked("COMPANY"));
 
     
+    // This line checks if the 
     modifier onlyRole(bytes32 _role){
         require (roles[_role][msg.sender], "not authorised");
         _;
     }
 
+    // need to modify constructor
     constructor() {
-        _grantRole(ADMIN, msg.sender);
+        _grantRole(USER, msg.sender);
     }
 
     function _grantRole(bytes32 _role, address _account) internal {
@@ -41,17 +43,16 @@ contract CustomerPreferences {
         emit GrantRole(_role, _account);
     }
 
-    function grantRole(bytes32 _role, address _account) external  onlyRole(ADMIN){
+    function grantRole(bytes32 _role, address _account) external  onlyRole(USER){
         _grantRole (_role,_account);
     }
 
-    function revokeRole(bytes32 _role, address _account) external  onlyRole(ADMIN){
+    function revokeRole(bytes32 _role, address _account) external  onlyRole(USER){
         roles[_role][_account] = false;
         emit RevokeRole(_role, _account);   
     }
 
-    // Prefrences Array
-    mapping(bytes32 => mapping ( address => bool) ) public prefrencess;
+
 
     // 0x57e249ee06eaace1940d50c53f83a2379596ee56f575e7b2da5612906fb1b610 // private
     bytes32 private constant SHOW = keccak256(abi.encodePacked("SHOW")); // should not be constant
